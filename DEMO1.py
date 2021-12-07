27dfd2ebaa99ce99228469d6375172d9b064b7fe
Hsun <jack0920123@gmail.com>
2021-12-07 01:45:44 -0800

    first commit

---
+++ DEMO1.py
@@ -0,0 +1,88 @@
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+import time
+
+#建立連線
+caps = {}
+caps['deviceName']= '127.0.0.1:62001'
+caps['platformName']= 'Android'
+caps['platformVersion']= '5.1.1'
+caps['appPackage']= 'com.blockchainvault'
+caps['appActivity']= 'com.pinetwork.MainActivity'
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub',caps)
+driver.implicitly_wait(5)
+#用電話登入
+el3 = driver.find_element_by_xpath(
+    "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View[4]/android.view.View/android.widget.TextView")
+el3.click()
+time.sleep(5)
+#點選國碼
+el4 = driver.find_element_by_id("android:id/text1")
+el4.click()
+driver.implicitly_wait(10)
+
+#拖拉下拉式選單
+source = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[1]')
+target = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[10]')
+TouchAction(driver).long_press(source).move_to(target).release().perform()
+driver.implicitly_wait(10)
+source = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[2]')
+target = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[10]')
+TouchAction(driver).long_press(source).move_to(target).release().perform()
+driver.implicitly_wait(10)
+driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[1]').click()
+time.sleep(5)
+#輸入手機號
+el6 = driver.find_element_by_xpath(
+    "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[3]/android.widget.EditText")
+el6.send_keys("933247900")
+time.sleep(5)
+#送出
+el7 = driver.find_element_by_xpath(
+    "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.widget.ScrollView/android.view.View/android.view.View[3]/android.view.View/android.view.View/android.widget.TextView")
+el7.click()
+driver.implicitly_wait(30)
+#輸入密碼
+e18 = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText')
+
+password = '@Cheere82113'
+e18.send_keys(password)
+# driver.press_keycode(77)
+# driver.press_keycode(31,64,59)
+# driver.press_keycode(36)
+# driver.press_keycode(32)
+# driver.press_keycode(32)
+# driver.press_keycode(46)
+# driver.press_keycode(32)
+# driver.press_keycode(15)
+# driver.press_keycode(9)
+# driver.press_keycode(8)
+# driver.press_keycode(8)
+# driver.press_keycode(10)
+
+
+# el8 = driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText")
+# el8.send_keys("@Cheere82113")
+driver.implicitly_wait(5)
+#送出
+el9 =  driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[4]/android.widget.Button")
+el9.click()
+driver.implicitly_wait(5)
+# time.sleep(10)
+
+try:
+    # 點擊挖幣
+    e20 = driver.find_element_by_xpath(
+        '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View[3]/android.view.View[3]')
+    e20.click()
+
+
+except:
+    #remind me later
+    e21 = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.webkit.WebView/android.webkit.WebView/android.view.View/android.app.Dialog/android.view.View/android.view.View/android.view.View[4]')
+    e21.click()
+
+    #點擊挖幣
+    e20 = driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View[3]/android.view.View[3]')
+    e20.click()
+
